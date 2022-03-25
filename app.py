5dce21b1b96cf8d87d6a79dcb6de3db2ba9b6122
Seth Bangert <seth.bangert@du.edu>
2022-03-24 23:03:32 -0600

    Change the head

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return "<h1>Hello World! Goodbyte Cruel World!</h1>"
+    return "<h1>Hello World! Welcome to the site! Goodbyte Cruel World!</h1>"
 
 
 if __name__ == '__main__':
