bb9ffee598fdbaec36e9c05edcbf76f3cb089e16
Seth Bangert <seth.bangert@du.edu>
2022-03-24 22:05:44 -0600

    initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return "<h1>Hello World!</h1>"
+
+
+if __name__ == '__main__':
+    app.run()
